744a877bff774783ec1fdf232ec027a480db4b78
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-09 00:28:53 -0500

    api utilities added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,22 @@
 import axios from "axios";
+	
+import filterParams from "./filterParams";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets articles from the NYT API
+  getArticles: function(params) {
+    return axios.get("/api/nyt", { params: filterParams(params) });
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets all saved articles
+  getSavedArticles: function() {
+    return axios.get("/api/articles");
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  // Deletes the saved article with the given id
+  deleteArticle: function(id) {
+    return axios.delete("/api/articles/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // Saves an article to the database
+  saveArticle: function(articleData) {
+    return axios.post("/api/articles", articleData);
   }
 };
